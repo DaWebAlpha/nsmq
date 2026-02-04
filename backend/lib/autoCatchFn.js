35ba131f15bb487e3af73346bc447970fcc18782
DeWebAlpha <tribillio@gmail.com>
2026-02-04 11:41:41 +0000

    added an error catcher

---
+++ backend/lib/autoCatchFn.js
@@ -0,0 +1,9 @@
+export default function autoCatchFn(fn){
+    return async function(req, res, next){
+        try{
+            await fn(req, res, next);
+        }catch(err){
+            next(err);
+        }
+    }
+}
